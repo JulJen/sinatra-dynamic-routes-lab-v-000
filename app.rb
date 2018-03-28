ce2a0ceefa94c273bd785d053b8d62cc9a6fe415
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 14:52:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
-    @reversename = paramas[:name].reverse!
+    @name = name.split('') = paramas[:name].reverse!
+    reverse = []
+
+    @name.each do |char|
+      @reversename = (reverse.unshift(char)).join('')
+    end 
     "#{@reversename}"
-  end 
+  end
 
 end
