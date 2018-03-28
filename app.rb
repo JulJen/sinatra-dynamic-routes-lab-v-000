fe09005f682759e7a10b13310ff841a62de396d3
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 15:17:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     #accepts five words and returns a string containing all five words
     @words = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
-    "#{@words}.to_s"
+    "#{@words}"
   end
 
     # get "/:operation/:number1/:number2" do
