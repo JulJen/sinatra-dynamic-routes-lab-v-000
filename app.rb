50c9bafbd9ba632be054c16b2ed3d7a3b5278280
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 15:15:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,5 +31,11 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     #accepts five words and returns a string containing all five words
     @words = params[:word1], params[:word2], params[:word2], params[:word2], params[:word2]
+  end
+
+    get "/:operation/:number1/:number2" do
+      #accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided
+      
+    end
 
 end
