faa7c0060da23686afdee5ab661f176c35211879
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 15:24:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,8 +30,10 @@ class App < Sinatra::Base
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     #accepts five words and returns a string containing all five words
-    @words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
-    @sentence = @words.join(' ')
+    # @words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
+    # @sentence = @words.join(' ')
+    # "#{@sentence}."
+    @sentence = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5] + "."
     "#{@sentence}."
   end
 
