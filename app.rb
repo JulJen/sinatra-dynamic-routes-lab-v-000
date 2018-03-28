4786885d0f1755359f283739d48b07a35fc08b4c
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 15:03:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @string = params[:phrase].to_s
-    @phrases = "#{@string} * #{@num} * ' '"
+    @phrases = "#{@num}".times do print "#{@string}" end
+
+    # @phrases = "#{@string} * #{@num} * ' '"
     "#{@phrases}"
   end
 
